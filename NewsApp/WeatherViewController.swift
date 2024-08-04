43e607df087e74074bcb893503938966e161c59d
svnelif <elifatsvn2003@gmail.com>
2024-08-04 23:06:07 +0300

    Uddated..

--- NewsApp/WeatherViewController.swift
+++ NewsApp/WeatherViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WeatherViewController.swift
 //  NewsApp
 //
 //  Created by Elif Ataseven  on 4.08.2024.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WeatherViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
