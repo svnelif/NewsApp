c2053883e9bbe82154c7d4ed2fa51ffece32640b
Elif Ataseven <elifatsvn2003@gmail.com>
2024-08-12 11:09:34 +0300

    Delete NewsApp/Weather directory

--- NewsApp/Weather/ForecastViiew.swift
+++
@@ -1,4 +0,0 @@
-import SwiftUI
-import WeatherKit
-import CoreLocation
-
--- NewsApp/Weather/Helpers/DataKeys.swift
+++
@@ -1,9 +0,0 @@
-
-import Foundation
-
-struct DataKeys {
-    static let locations = "Locations"
-    static let locationCount = "LocationCount"
-    static let lastViewedPage = "LastViewedPage"
-    static let temperatureUnit = "TemperatureUnit"
-}
