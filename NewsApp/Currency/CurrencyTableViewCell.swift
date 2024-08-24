0fe11b34e0110ac5c6f5ac68846051c5aceaf479
svnelif <elifatsvn2003@gmail.com>
2024-08-24 23:49:26 +0300

    Updated.

---
+++ NewsApp/Currency/CurrencyTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CurrencyTableViewCell.swift
+//  NewsApp
+//
+//  Created by Elif Ataseven  on 24.08.2024.
+//
+
+import UIKit
+
+class CurrencyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
