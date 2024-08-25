24ee62f197d2ab948d7c019b5a8dfd36ce7475bb
svnelif <elifatsvn2003@gmail.com>
2024-08-25 23:50:18 +0300

    Updated.

--- NewsApp/SideBar/LanguageViewController.swift
+++ NewsApp/SideBar/LanguageViewController.swift
@@ -1,7 +1,10 @@
 import UIKit
+import SideMenu
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UIViewController, MenuListControllerDelegate {
 
+    var menu: SideMenuNavigationController?
+    
     private let chooseLanguageLabel: UILabel = {
         let label = UILabel()
         label.text = "Choose Language".localized
@@ -45,6 +48,7 @@ class SettingsViewController: UIViewController {
         view.backgroundColor = .systemBackground
         setupViews()
         setupSwitches()
+        setupMenu()
     }
     
     private func setupViews() {
@@ -108,5 +112,33 @@ class SettingsViewController: UIViewController {
             window.rootViewController = initialViewController
             window.makeKeyAndVisible()
         }
+    }
+    
+    @IBAction func didSettings(_ sender: Any) {
+        present(menu!, animated: true)
+    }
+    
+    func didSelectMenuItem(named: SideMenuItem) {
+        menu?.dismiss(animated: true, completion: { [weak self] in
+            var viewController: UIViewController?
+            switch named {
+            case .language:
+                viewController = SettingsViewController()
+            }
+            if let vc = viewController {
+                self?.present(vc, animated: true, completion: nil)
+            }
+        })
+    }
+
+    private func setupMenu() {
+        var menuItem = MenuListController(with: SideMenuItem.allCases)
+        menu = SideMenuNavigationController(rootViewController: menuItem)
+        
+        menuItem.delegate = self
+        menu?.leftSide = true
+        menu?.setNavigationBarHidden(true, animated: true)
+        SideMenuManager.default.leftMenuNavigationController = menu
+        SideMenuManager.default.addPanGestureToPresent(toView: self.view)
     }
 }
